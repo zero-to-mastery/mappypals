66ee1d58de939140a0119e161c0272286f63518f
Marco <sciortino.mrc@gmail.com>
2019-04-09 15:32:07 +0100

    fix on pop up forms

--- src/pages/Home/Map.js
+++ src/pages/Home/Map.js
@@ -7,7 +7,6 @@ const TOKEN =
   "pk.eyJ1Ijoic2Npb3J0aW5vbXJjIiwiYSI6ImNqc2RocmRzYTB2OGUzeWxuZDNmdDhrcDgifQ.txLXHEJPl4lYa8an6fcjuA";
 class Map extends Component {
   state = {
-    edit: false,
     currentPin: {},
     newFriend: {
       long: null,
@@ -27,14 +26,8 @@ class Map extends Component {
       latitude: 37.7577,
       longitude: -122.4376,
       zoom: 11
-    },
-    //user pin
-    pinMe:{
-      latitude: "",
-      longitude: "",
     }
   };
-
   getPostcode = () => {
     fetch(
       "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
@@ -58,30 +51,28 @@ class Map extends Component {
   getLocation = event => {
     const lngLat = event.lngLat;
     this.setState({
-      newFriend: { ...this.state.newFriend, long: lngLat[0], lat: lngLat[1] },
+      newFriend: { ...this.state.newFriend, long: lngLat[0], lat: lngLat[1] }
     });
-   setTimeout(()=>console.log(this.state.newFriend),500)
   };
 
   showPopup = event => {
-    this.setState({ currentPin: this.state.friendsList[event.target.id]});
+    this.setState({ currentPin: this.state.friendsList[event.target.id] });
     setTimeout(() => {
       const popup = document.getElementById("popup");
       if (popup) popup.style.height = "180px";
     }, 500);
   };
   hidePopup = event => {
-    if(!this.state.edit){
-      const popup = document.getElementById("popup");
-      console.log(this.state.currentPin)
-      popup.style.height = "0px";
-      setTimeout(()=>this.setState({ currentPin: {} }),600);
-    }
+    const popup = document.getElementById("popup");
+    if (this.state.currentPin.long) popup.style.height = "0px";
+    this.setState({ currentPin: {} });
   };
+  //temporary "database insert"
   nicknameExists = (nickname, email) => {
     const addNew = document.getElementById("add-new");
     const hashListKeys = Object.keys(this.state.friendsList);
     let flag = false;
+    console.log({ nickname, email });
     if (!nickname || !nickname.length || hashListKeys.includes(nickname)) {
       addNew.children[0].style.border = "2px solid red";
       flag = true;
@@ -100,8 +91,6 @@ class Map extends Component {
     }
     return flag;
   };
-  //temporary "database insert"
-
   addFriendData = friendState => {
     const { nickname, name, email, phone } = friendState.data;
     if (this.nicknameExists(nickname, email)) return;
@@ -126,15 +115,20 @@ class Map extends Component {
         ...this.state.friendsList,
         [this.state.newFriend.nickname]: this.state.newFriend
       },
-      newFriend: resetNewFriend,
-      edit: false,
-      show: false
+      newFriend: resetNewFriend
     });
+    const addNew = document.getElementById("add-new");
+    addNew.style.height = "0px";
+    for (let child of addNew.children) {
+      child.value = "";
+    }
   };
 
   newPin = event => {
     this.getLocation(event);
     this.getPostcode();
+    const addNew = document.getElementById("add-new");
+    addNew.style.height = "180px";
   };
 
   componentDidMount() {
@@ -146,18 +140,14 @@ class Map extends Component {
               ...this.state.viewport,
               latitude: position.coords.latitude,
               longitude: position.coords.longitude
-            },
-            pinMe:{
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude
-            },
+            }
           });
       }
     );
       window.addEventListener("resize", viewport => {
       this.setState({
         viewport: {
-          ...this.state.viewport,
+          ...viewport,
           width: viewport.target.innerWidth,
           height: viewport.target.innerHeight
         }
@@ -180,7 +170,7 @@ class Map extends Component {
           <i
             id={nickname}
             key={nickname + "k"}
-            onClick={this.activateEditMode}
+            onClick={() => console.log("Here to edit friends details")}
             onMouseOver={e => this.showPopup(e)}
             onMouseLeave={e => this.hidePopup(e)}
             className="fas fa-map-marker-alt"
@@ -190,27 +180,6 @@ class Map extends Component {
     }
     return htmlMarkersCollection;
   };
-  activateEditMode=()=>{
-    this.setState({edit: true})
-  }
-  closeEditMode=()=>{
-    this.setState({edit: false})
-  }
-
-  handleEdit = (friend) => {
-    this.setState({newFriend: friend, edit: true, show: true });
-  }
-
-  renderForm() {
-    if(this.state.show === true) {
-      return(
-        <AddFriendForm onFriendLoaded={this.addFriendData} data={this.state.newFriend} edit={this.state.edit} show={this.state.show} onCloseClick={this.onClose} />
-      )
-    } else {
-      return null;
-    }
-    
-  }
 
   render() {
     return (
@@ -233,32 +202,20 @@ class Map extends Component {
           ) : (
             ""
           )}
-          {this.state.pinMe.latitude?
-            <Marker key="me" latitude={this.state.pinMe.latitude} longitude={this.state.pinMe.longitude}>
-              <i
-                id="me"
-                key="me-pin"
-                className="fas fa-map-marker-alt"
-              />
-            </Marker>:""
-          }
         </MapGL>
         {this.state.currentPin.nickname && !this.state.newFriend.long ? (
           <PopUp
-            editMode={this.state.edit}
             nickname={this.state.currentPin.nickname}
             name={this.state.currentPin.name}
             email={this.state.currentPin.email}
             phone={this.state.currentPin.phone}
             postcode={this.state.currentPin.postcode}
-            save={this.closeEditMode}
-            hideThis={this.hidePopup}
           />
         ) : (
           ""
         )}
         {/*this is for now fully visible still being implemented*/}
-        {this.renderForm()}
+        <AddFriendForm onFriendLoaded={this.addFriendData} />
       </div>
     );
   }
