16f935ff7995c5bdd9d908e3dd6f731cac5a432a
Interior Nature <klsnow365@gmail.com>
2019-04-22 18:19:46 -0400

    PasswordReqs added

--- src/pages/Login/Signup.js
+++ src/pages/Login/Signup.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-
 import { Link } from 'react-router-dom';
-import Form from './Form.js';
+import Form, { PasswordReqs } from './Form.js';
 import './Login.css';
 import axios from 'axios'
 
