38a7715ca0772fefc1948f2ad35259c6ebf20792
Arnas <1arnasdickus1@gmail.com>
2019-04-29 22:18:06 +0300

    first navbar draft. On map not working hamburger icon badly placed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,24 +8,79 @@
 
 */
 
-import React from 'react';
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 import NavLogo from "../../pics/Logo.svg";
 import Team from "../../pics/Team.svg";
 import About from "../../pics/AboutUs.svg";
 
+ class Navbar extends Component {
+   constructor(props){
+     super(props);
+     this.state = {
+       navbarActive: false,
+     };
+   };
+   toggleClass = () => {
+     this.setState({
+       navbarActive: !this.state.navbarActive
+     })
+   }
+  render() {
+    return (
+    <React.Fragment>
+      <header>
+        <div className="nav-wrapper">
+          <div className = "hamburger-icon" onClick = {this.toggleClass}>
+            <div className = "hamburger-middle"></div>
+            <div className = "hamburger-end"></div>
+          </div>
+          <div className = {this.state.navbarActive === true ? "navbar navbarToggle" : "navbar"}>
+            <div className='nav-item'>
+                <img src={NavLogo} alt="Logo" />
+                <div className = "mainTitle">MappyPals</div>
+            </div>
+            <div className='nav-item' >
+                  <img src={Team} alt="Team"  />
+                  <div className = "title">Team</div>
+            </div>
+            <div className='nav-item'>
+                  <img src={About} alt="About"  />
+                  <div className = "title">About</div>
+            </div>
+            <div className='nav-item'>
+                  <div className='login'>
+                    <Link className = "button" to='/Login' >Login</Link>
+                  </div>
+                  <div className='signup'>
+                    <Link className='button' to='/SignUp' >Sign Up</Link>
+                  </div>
+            </div>
+            <p>Copyright - Team <br/> Members - ZTM Link <br/> etc</p>
+          </div>
+        </div>
+      </header>
+    </React.Fragment>
+    )
+  }
+}
 
+export default Navbar;
+
+
+/*
 const Navbar=({isUserLogged, onLogin})=>{
   return(
   <React.Fragment>
     
     <header>
       <div className="nav-wrapper">
-      <div className = "hamburger-icon hamburger-display">
-        <div className = "hamburger-middle"></div>
-        <div className = "hamburger-end"></div>
-      </div>
+        <div className = "hamburger-icon" onClick = {}>
+          <div className = "hamburger-middle"></div>
+          <div className = "hamburger-end"></div>
+        </div>
+        <div class = 'navbar'>
           <div className='nav-item'>
               <img src={NavLogo} alt="Logo" />
               <div className = "mainTitle">MappyPals</div>
@@ -47,6 +102,7 @@ const Navbar=({isUserLogged, onLogin})=>{
                 </div>
           </div>
           <p>Copyright - Team <br/> Members - ZTM Link <br/> etc</p>
+        </div>
       </div>
     </header>
   </React.Fragment>
@@ -54,3 +110,4 @@ const Navbar=({isUserLogged, onLogin})=>{
 }
 
 export default Navbar
+*/
