679ec9e6c47f4e7040a2dd49ae11fbdd5990e179
Interior Nature <klsnow365@gmail.com>
2019-05-02 17:46:16 -0400

    forgot password from login page

--- src/pages/Login/ResetPassword.js
+++ src/pages/Login/ResetPassword.js
@@ -1,33 +1,67 @@
-
-
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Form from './Form.js';
 import './Login.css';
-import Form from './Form';
-import './ResetPassword.css';
+import axios from 'axios';
 
- class ResetPassword extends Component {
-     
+class ResetPassword extends Component {
+    constructor(props) {
+        super(props);  
+    }
+
+  // Submitted email
+    handleSubmit = event => {
+        event.preventDefault();
+        const url = 'http://localhost:3001/users/reset'
+        axios({
+            url: url,
+            method: 'POST',
+            data: JSON.stringify(this.state),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(res => {
+            if (res.status === 200 || res.data.redirect) {
+                //Write redirect logic here
+                console.log("Redirect user to main page");
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            console.log('Please check entry and try again, or go back to Login to sign up');
+        });
+        
+        // Clear inputs.
+        this.setState({email: ''});
+    }
+
   render() {
     return (
-    <div className = "Login">
-        <Form onSubmit = {this.handleSubmit} className = "max-width600">
-            <p className = "text-center text-large">Did you forget your password?</p>
-            <p className = "text-center text-medium">Enter the email address from your account below and 
-                we will send your password reset link.
-            </p>
-            <div>
-                <label htmlFor="Email">Email
-                    <input 
-                        type="email" 
-                        name = "email"
-                        placeholder = ""
-                        onChange={this.handleChange} required 
-                    />
-                </label>
+        <div className="Login">
+        <Form onSubmit={this.handleSubmit}>
+            <div className="text-center-wrap">
+                <div className="heavy">
+                    <p>Did you forget your password?</p>
+                </div>
+                <div className="light">
+                    <p>Enter your mappypals email address, we'll send you a password reset link.</p>
+                </div>
             </div>
+            <label htmlFor="email" className="heavy">
+                Email
+                <input
+                type="email"
+                name="email"
+                onChange={this.handleChange} required
+                />
+            </label>
             <div className="btnContainer">
-                <button className = "buttonReset" type="submit" >RESET PASSWORD</button>
+                <button type="submit">Reset Password</button>
             </div>
+            <p className = 'u-text-center'> 
+                <Link className="nav-item" to='/login'> Back to Login </Link>
+            </p>
         </Form>
     </div>
     )
