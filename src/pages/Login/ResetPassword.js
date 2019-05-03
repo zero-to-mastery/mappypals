94f6ada3e4d21ef6beef1537aa26f22a52b7c3bb
Interior Nature <klsnow365@gmail.com>
2019-05-03 09:05:53 -0400

    fixed warning that travis cli rejected

--- src/pages/Login/ResetPassword.js
+++ src/pages/Login/ResetPassword.js
@@ -5,10 +5,6 @@ import './Login.css';
 import axios from 'axios';
 
 class ResetPassword extends Component {
-    constructor(props) {
-        super(props);  
-    }
-
   // Submitted email
     handleSubmit = event => {
         event.preventDefault();
