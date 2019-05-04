0bb17ba7946f72708100b3c176b3610ed88c1504
heyitsaking <rey.arturo95@gmail.com>
2019-05-04 00:32:18 -0400

    Navbar changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,17 +7,24 @@ const Navbar=({isUserLogged, onLogin})=>{
   return(
   <React.Fragment>
       <header>
-          <div id="logo-wrapper">
-          <Link to="/">
-            {/*<p id="logo"> <span>Mappy&nbsp; als  &nbsp;</span> </p>
-            <i className="fas fa-map-marker-alt pin"></i>*/}
-            <img src={logo} alt="Mappy Pals"/>
-          </Link>
+        <Link to="/">
+          <div id="logo_title">
+            <span id="logo-wrapper">            
+              {/*<p id="logo"> <span>Mappy&nbsp; als  &nbsp;</span> </p>
+              <i className="fas fa-map-marker-alt pin"></i>*/}
+              <img src={logo} alt="Mappy Pals"/>
+            </span>
+            <span id="title">
+              <span>mappypals</span>
+            </span>
           </div>
+        </Link>
           <div id="nav-wrapper">
-            <Link className="nav-item nonLogin" to='/'> HOME </Link>
-            <Link className="nav-item nonLogin" to='/about'> ABOUT </Link>
-            <Link className="nav-item nonLogin" to='/contact'> CONTACT </Link>
+              <div id="nonLogin">
+                <Link className="nav-item " to='/'> <div className="underline-hover">HOME</div> </Link>
+                <Link className="nav-item " to='/about'> <div className="underline-hover">ABOUT</div> </Link>
+                <Link className="nav-item " to='/contact'> <div className="underline-hover">CONTACT</div> </Link>
+              </div>
             <Link className="nav-item login" to='/login'> <p onClick={onLogin}> SIGN IN </p> </Link>
           </div>
       </header>
