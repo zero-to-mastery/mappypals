ec8f534924064b8d00cf3f557e40aaf711d28f82
Mubarak Showole <mubarakshow@github.com>
2019-05-21 11:32:40 +0100

    fixed user external links

--- src/pages/About/team-data.js
+++ src/pages/About/team-data.js
@@ -22,15 +22,15 @@ const location = [
 ]
 
 const team = [
-  { avatar: null, name: 'Mubarak Show', role: `${role[4]+', '+role[8]}`, location: location[0], isEmployed: false, portfolio: "www.google.com/", linkedin: "www.linkedin.com" },
-  { avatar: null, name: 'Marco Sciortino', role: role[2], location: location[1], isEmployed: false, portfolio: "www.google.com/", linkedin: "www.linkedin.com" },
-  { avatar: null, name: 'Arnas Dičkus', role: role[6], location: location[5], isEmployed: false, portfolio: "www.google.com/", linkedin: "www.linkedin.com" },
-  { avatar: null, name: 'Yash Puthran', role: role[1], location: location[6], isEmployed: true, portfolio: "www.google.com/", linkedin: "www.linkedin.com" },
-  { avatar: null, name: 'Thijs Nijhof', role: `${role[5]}, ${role[0]}`  , location: location[1], isEmployed: false, portfolio: "www.google.com/", linkedin: "www.linkedin.com" },
-  { avatar: null, name: 'Kristina', role: role[2], location: location[1], isEmployed: false, portfolio: "www.google.com/", linkedin: "www.linkedin.com" },
-  { avatar: null, name: 'Melissendra', role: role[0], location: location[1], isEmployed: false, portfolio: "www.google.com/", linkedin: "www.linkedin.com" },
-  { avatar: null, name: 'Matt Smith', role: role[3], location: location[1], isEmployed: false, portfolio: "www.google.com/", linkedin: "www.linkedin.com" },
-  { avatar: null, name: 'Yassine Belkaid', role: role[1], location: location[7], isEmployed: false, portfolio: "www.google.com/", linkedin: "www.linkedin.com" },
+  { avatar: null, name: 'Mubarak Show', role: `${role[4]+', '+role[8]}`, location: location[0], isEmployed: false, portfolio: "https://www.google.com/", linkedin: "https://www.linkedin.com/" },
+  { avatar: null, name: 'Marco Sciortino', role: role[2], location: location[1], isEmployed: false, portfolio: "https://www.google.com//", linkedin: "https://www.linkedin.com/" },
+  { avatar: null, name: 'Arnas Dičkus', role: role[6], location: location[5], isEmployed: false, portfolio: "https://www.google.com//", linkedin: "https://www.linkedin.com/" },
+  { avatar: null, name: 'Yash Puthran', role: role[1], location: location[6], isEmployed: true, portfolio: "https://www.google.com//", linkedin: "https://www.linkedin.com/" },
+  { avatar: null, name: 'Thijs Nijhof', role: `${role[5]}, ${role[0]}`  , location: location[1], isEmployed: false, portfolio: "https://www.google.com//", linkedin: "https://www.linkedin.com/" },
+  { avatar: null, name: 'Kristina', role: role[2], location: location[1], isEmployed: false, portfolio: "https://www.google.com//", linkedin: "https://www.linkedin.com/" },
+  { avatar: null, name: 'Melissendra', role: role[0], location: location[1], isEmployed: false, portfolio: "https://www.google.com//", linkedin: "https://www.linkedin.com/" },
+  { avatar: null, name: 'Matt Smith', role: role[3], location: location[1], isEmployed: false, portfolio: "https://www.google.com//", linkedin: "https://www.linkedin.com/" },
+  { avatar: null, name: 'Yassine Belkaid', role: role[1], location: location[7], isEmployed: false, portfolio: "https://www.google.com//", linkedin: "https://www.linkedin.com//" },
 ]
 
 
