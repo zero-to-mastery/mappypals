5b589e55c981b62393e7e73b5560785039d074a0
Arnas <1arnasdickus1@gmail.com>
2019-06-13 15:15:28 +0300

    added a few styles

--- src/pages/Home/InviteFriends/InviteFriends.js
+++ src/pages/Home/InviteFriends/InviteFriends.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-
+import Form from '../../Login/Form';
+import classes from './InviteFriends.module.css';
 class InviteFriends extends Component {
     render() {
         return (
-            <div>
-                <h1>This is an invite</h1>
+            <div className={classes.container}>
+                <Form onSubmit={this.handleSubmit} className={classes.form}>
+                    <h2 className={classes.title}>
+                        Send an invite to multiple friends
+                    </h2>
+                </Form>
             </div>
         );
     }
