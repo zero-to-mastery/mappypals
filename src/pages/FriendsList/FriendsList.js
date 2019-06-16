c38a84212b74f80901d517b03b2094b8f995fbd3
Ante Plazanic <antezg223@gmail.com>
2019-06-16 17:26:28 +0200

    Removed: import Layout from '../../components/Layout'

--- src/pages/FriendsList/FriendsList.js
+++ src/pages/FriendsList/FriendsList.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Layout from '../../components/Layout'
 import friends from './friends-data'
 import FriendCard from './FriendCard';
 import SearchFriends from './SearchFriends';
