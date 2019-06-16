957ca12c37508f23e11af95f57400077270caae1
Ante Plazanic <antezg223@gmail.com>
2019-06-16 17:32:53 +0200

    Removed all the comments since the logic behind filtering is pretty obvious

--- src/pages/FriendsList/FriendsList.js
+++ src/pages/FriendsList/FriendsList.js
@@ -41,35 +41,29 @@ class FriendsList extends React.Component {
 
 
 handleChange = e => {
-  // Variable to hold the original version of the list
 let currentList = friends;
-  // Variable to hold the filtered list before putting into state
 let newList = [];
 
-  // If the search bar isn't empty
-if (e.target.value !== "") {
-      // Assign the original list to currentList
-//currentList = friends;
 
-      // Use .filter() to determine which items should be displayed
-      // based on the search terms
+if (e.target.value !== "") {
+
+
+
 newList = currentList.filter(item => {
-          // change current item to lowercase
+    
   const lc = item.name.toLowerCase();
-          // change search term to lowercase
+    
   const filter = e.target.value.toLowerCase();
-          // check to see if the current list item includes the search term
-          // If it does, it will be added to newList. Using lowercase eliminates
-          // issues with capitalization in search terms and search content
+        
           
           
   return lc.includes(filter);
 });
 } else {
-      // If the search bar is empty, set newList to original task list
+      
 newList = friends;
 }
-  // Set the filtered state based on what our rules added to newList
+  
 this.setState({
 friends: newList
 });
