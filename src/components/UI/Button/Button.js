1133ba6b2ee7333765da6dd047e86f570ebe8204
Arnas <1arnasdickus1@gmail.com>
2019-06-24 16:53:16 +0300

    Added buttons to invitation sent

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -5,7 +5,7 @@ const button = props => {
     return (
         <button
             className={[classes.Button, classes[props.btnType]].join(' ')}
-            onClick={props.clicked}
+            onClick={props.onClick}
         >
             {props.children}
         </button>
