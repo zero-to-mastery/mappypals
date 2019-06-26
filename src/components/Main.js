e289afab12a9766351e7b989c7f40b0eb14cd6c1
Arnas <1arnasdickus1@gmail.com>
2019-06-26 18:17:10 +0300

    changing class login to signup

--- src/components/Main.js
+++ src/components/Main.js
@@ -26,7 +26,7 @@ const Main = () => (
             <Route path="/contact" component={Contact} />
             <Route path="/forgotpassword" component={ForgotPassword} />
             <Route path="/friendslist" component={FriendsList} />
-            <Route path="/resetpassword" component={ResetPassword} />
+            <Route path="/resetpassword" component={ResetPassword} /> *
             <Route
                 path="/settings/profilesettings"
                 component={ProfileSettings}
--- src/pages/Login/Signup.js
+++ src/pages/Login/Signup.js
@@ -7,7 +7,7 @@ import './Login.css';
 import ky from 'ky';
 import Button from '../../components/UI/Button/Button';
 
-class Login extends Component {
+class Signup extends Component {
     constructor(props) {
         super(props);
         window.FB.init({
@@ -56,6 +56,7 @@ class Login extends Component {
                 const url = 'http://localhost:3001/users/register';
                 await ky.post(url, { json: this.state }).then(res => {
                     if (res.status === 200) {
+                        console.log(res);
                         this.props.history.push('/login');
                     }
                 });
@@ -180,4 +181,4 @@ class Login extends Component {
     }
 }
 
-export default Login;
+export default Signup;
