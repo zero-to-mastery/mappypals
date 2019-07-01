746e7c217ec15994781b3e03467e4faa45a98860
Arnas <1arnasdickus1@gmail.com>
2019-07-01 13:51:14 +0300

    finished styling

--- src/pages/Home/Popup/Popup.js
+++ src/pages/Home/Popup/Popup.js
@@ -16,9 +16,19 @@ class Popup extends PureComponent {
                             {info.firstName} {info.lastName}
                         </p>
                     </div>
-                    <div>
-                        <button className={classes.button}>Profile</button>
-                        <button className={classes.button}>Message</button>
+                    <div className={classes.buttonContainer}>
+                        <button className={classes.button}>
+                            <i className={`${classes.icon} far fa-user`} />
+                            Profile
+                        </button>
+                        <button className={classes.button}>
+                            <i
+                                className={`${
+                                    classes.icon
+                                } far fa-comment-dots`}
+                            />
+                            Message
+                        </button>
                     </div>
                 </div>
                 <p className={classes.description}>This is description</p>
