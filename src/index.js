33a390d57aa527e950165861ae5ca246c95e653f
CongLiu <52264917+CongLiu-CN@users.noreply.github.com>
2019-07-21 21:56:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import dotenv from 'dotenv';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
