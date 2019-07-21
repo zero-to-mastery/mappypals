13a6ffc046553172f2777aa0975aaaa2d5441508
CongLiu <52264917+CongLiu-CN@users.noreply.github.com>
2019-07-21 21:55:29 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
 
-dotenv.config();
-
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
