50b4ab6c9ad073a13dc7211f57cddcebf82477c3
comphonia <online.comphonia@gmail.com>
2019-08-13 00:39:43 -0500

    file handler, layout re-design, new avatar

--- src/pages/Settings/ProfileSettings/ProfileSettings.js
+++ src/pages/Settings/ProfileSettings/ProfileSettings.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classes from './ProfileSettings.module.css';
-import defaultProfilePicture from '../../../pics/default-profile-pic.jpg';
+import defaultProfilePicture from '../../../pics/blank-profile-picture.png';
 import DisplayInterestsImport from './displayInterests/displayInterests';
 import SettingsNavbar from '../SettingsNavbar/SettingsNavbar';
 import Button from '../../../components/UI/Button/Button';
@@ -8,7 +8,7 @@ import Button from '../../../components/UI/Button/Button';
 class ProfileSettings extends Component {
     state = {
         displayUploadError: false,
-        displayDublicateError: false,
+        displayDuplicateError: false,
         displayEmptyInterestError: false,
         // User inputed interest
         Interests: '',
@@ -20,20 +20,29 @@ class ProfileSettings extends Component {
             AboutMe: '',
             storeAllInterests: [],
             selectedFile: null
-        }
+        },
+        avatarURI: null
     };
-    fileSelectedHandler = event => {
+    fileSelectedHandler = e => {
         const string = 'image';
-        let svg = event.target.files[0].type.includes('image/svg+xml');
-        let gif = event.target.files[0].type.includes('image/gif');
-        if (event.target.files[0].type.includes(string) && !svg && !gif) {
-            this.hideUploadError();
-            this.setState({
-                changedProfileData: {
-                    ...this.state.changedProfileData,
-                    selectedFile: event.target.files[0]
+        if (e.target.files && e.target.files[0]) {
+            let file = e.target.files[0];
+            let svg = file.type.includes('image/svg+xml');
+            let gif = file.type.includes('image/gif');
+            if (file.type.includes(string) && !svg && !gif) {
+                this.hideUploadError();
+                let reader = new FileReader();
+                reader.onload = ev=>{
+                    this.setState({
+                        changedProfileData: {
+                            ...this.state.changedProfileData,
+                            selectedFile: file.name
+                        },
+                        avatarURI:ev.target.result
+                    });
                 }
-            });
+                reader.readAsDataURL(e.target.files[0]);
+            }
         } else {
             this.displayUploadError();
         }
@@ -44,14 +53,14 @@ class ProfileSettings extends Component {
         console.log(this.state.changedProfileData);
     };
     addInterest = () => {
-        let isDublicate = this.isInterestDublicate();
+        let isDuplicate = this.isInterestDuplicate();
 
         if (this.state.Interests === '') {
             this.displayEmptyInterestError();
-        } else if (isDublicate) {
-            this.displayDublicateError();
+        } else if (isDuplicate) {
+            this.displayDuplicateError();
         } else {
-            this.hideDublicateError();
+            this.hideDuplicateError();
             this.storeInterests();
         }
     };
@@ -78,11 +87,11 @@ class ProfileSettings extends Component {
             }
         );
     };
-    isInterestDublicate = () => {
-        let isDublicate = this.state.storeInterests.find(
+    isInterestDuplicate = () => {
+        let isDuplicate = this.state.storeInterests.find(
             interest => interest === this.state.Interests
         );
-        return isDublicate;
+        return isDuplicate;
     };
     removeInterest = data => {
         const interestsToKeep = this.state.storeInterests.filter(
@@ -98,9 +107,9 @@ class ProfileSettings extends Component {
     displayUploadError = () => this.setState({ displayUploadError: true });
     hideUploadError = () => this.setState({ displayUploadError: false });
 
-    displayDublicateError = () =>
-        this.setState({ displayDublicateError: true });
-    hideDublicateError = () => this.setState({ displayDublicateError: false });
+    displayDuplicateError = () =>
+        this.setState({ displayDuplicateError: true });
+    hideDuplicateError = () => this.setState({ displayDuplicateError: false });
 
     cleanInterestState = () => this.setState({ Interests: '' });
 
@@ -131,9 +140,9 @@ class ProfileSettings extends Component {
             uploadError = <p>This type of file is not supported</p>;
         }
 
-        let dublicateError = '';
-        if (this.state.displayDublicateError) {
-            dublicateError = <p>This interest already exist</p>;
+        let DuplicateError = '';
+        if (this.state.displayDuplicateError) {
+            DuplicateError = <p>This interest already exist</p>;
         }
         let emptyInterest = '';
         if (this.state.displayEmptyInterestError) {
@@ -149,7 +158,11 @@ class ProfileSettings extends Component {
                 >
                     <div className={classes.pictureItem}>
                         <img
-                            src={defaultProfilePicture}
+                            src={
+                                this.state.avatarURI
+                                    ? this.state.avatarURI
+                                    : defaultProfilePicture
+                            }
                             alt=""
                             className={classes.img}
                         />
@@ -159,91 +172,97 @@ class ProfileSettings extends Component {
                             onChange={this.fileSelectedHandler}
                             ref={fileInput => (this.fileInput = fileInput)}
                         />
-                        <p
+                        <div
                             className={classes.changePicture}
                             onClick={() => this.fileInput.click()}
                         >
-                            Change picture
-                        </p>
+                            <p>Change picture </p>
+                        </div>
                         {uploadError}
                     </div>
                     <div className={classes.form}>
-                        <div className={classes.nameContainer}>
+                        <div>
+                            <div className={classes.nameContainer}>
+                                <div>
+                                    <label htmlFor="Firstname">Firstname</label>
+                                    <br />
+                                    <input
+                                        type="text"
+                                        name="FirstName"
+                                        className={[
+                                            classes.input,
+                                            classes.u_width_80
+                                        ].join(' ')}
+                                        onChange={this.handleChangeObject}
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="Lastname">Lastname</label>
+                                    <br />
+                                    <input
+                                        type="text"
+                                        name="Lastname"
+                                        className={[
+                                            classes.input,
+                                            classes.u_width_80
+                                        ].join(' ')}
+                                        onChange={this.handleChangeObject}
+                                    />
+                                </div>
+                            </div>
                             <div>
-                                <label htmlFor="Firstname">Firstname</label>
-                                <br />
-                                <input
-                                    type="text"
-                                    name="FirstName"
+                                <p>About me</p>
+                                <textarea
                                     className={[
                                         classes.input,
-                                        classes.u_width_80
+                                        classes.u_width_80,
+                                        classes.inputTextarea
                                     ].join(' ')}
                                     onChange={this.handleChangeObject}
                                 />
                             </div>
                             <div>
-                                <label htmlFor="Lastname">Lastname</label>
-                                <br />
+                                <p>Interests</p>
                                 <input
                                     type="text"
-                                    name="Lastname"
-                                    className={[
-                                        classes.input,
-                                        classes.u_width_80
-                                    ].join(' ')}
-                                    onChange={this.handleChangeObject}
+                                    name="Interests"
+                                    className={classes.input}
+                                    onChange={this.handleChange}
                                 />
+                                <Button
+                                    btnType="ProfileSettingsInterests"
+                                    onClick={this.addInterest}
+                                >
+                                    Add
+                                </Button>
+                                {DuplicateError}
+                                {emptyInterest}
+                                <div className={classes.displayInterests}>
+                                    {this.state.storeInterests.map(
+                                        (data, index) => {
+                                            return (
+                                                <DisplayInterestsImport
+                                                    key={index}
+                                                    info={data}
+                                                    removeInterest={() =>
+                                                        this.removeInterest(
+                                                            data
+                                                        )
+                                                    }
+                                                />
+                                            );
+                                        }
+                                    )}
+                                </div>
                             </div>
-                        </div>
-                        <div>
-                            <p>About me</p>
-                            <textarea
-                                name="AboutMe"
-                                cols="30"
-                                rows="10"
-                                onChange={this.handleChangeObject}
-                            />
-                        </div>
-                        <div>
-                            <p>Interests</p>
-                            <input
-                                type="text"
-                                name="Interests"
-                                className={classes.input}
-                                onChange={this.handleChange}
-                            />
-                            <Button
-                                btnType="ProfileSettingsInterests"
-                                onClick={this.addInterest}
-                            >
-                                Add
-                            </Button>
-                            {dublicateError}
-                            {emptyInterest}
-                            <div className={classes.displayInterests}>
-                                {this.state.storeInterests.map(
-                                    (data, index) => {
-                                        return (
-                                            <DisplayInterestsImport
-                                                key={index}
-                                                info={data}
-                                                removeInterest={() =>
-                                                    this.removeInterest(data)
-                                                }
-                                            />
-                                        );
-                                    }
-                                )}
+                            <div className={classes.submitContainer}>
+                                <Button
+                                    btnType="ProfileSettingsInterests"
+                                    type="submit"
+                                >
+                                    Save Changes
+                                </Button>
                             </div>
-                        </div>
-                        <div className={classes.submitContainer}>
-                            <Button
-                                btnType="ProfileSettingsInterests"
-                                type="submit"
-                            >
-                                Save Changes
-                            </Button>
                         </div>
                     </div>
                 </form>
